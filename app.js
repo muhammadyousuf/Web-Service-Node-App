5319bc665701b2bfac328d620b865e24b51a5661
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-10-28 23:36:29 +0500

    add partial and hbs register helper

--- app.js
+++ app.js
@@ -1,42 +1,44 @@
-const express = require('express');
-const hbs = require('hbs')
+const express = require("express");
+const hbs = require("hbs");
 
 var app = express();
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"));
 
-app.set('view engine', 'hbs')
-app.use(express.static(__dirname + '/public'));
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
 
-app.get('/', (req, res, next) => {
-    res.send('<h1>Hello Express</h1>');
+hbs.registerHelper("screamIt", text => {
+  return text.toUpperCase();
+});
 
-})
+app.get("/", (req, res, next) => {
+  res.send("<h1>Hello Express</h1>");
+});
 
-app.get('/about', (req, res, next) => {
-    res.render('about.hbs', {
-        pageTile: 'About Page',
-        currentYear: new Date().getFullYear()
-    })
+app.get("/about", (req, res, next) => {
+  res.render("about.hbs", {
+    pageTile: "About Page"
+  });
+});
 
-})
+app.get("/express", (req, res, next) => {
+  res.render("express.hbs", {
+    firstName: "Muhammad",
+    lastName: "Yousuf",
+    likes: ["FaceBook Instagram Whatsapp"],
+    pageTile: "Express Page"
+  });
+});
 
-app.get('/express', (req, res, next) => {
-    res.render('express.hbs', {
-        firstName: 'Muhammad',
-        lastName:'Yousuf',
-        likes: ['FaceBook Instagram Whatsapp'],
-        pageTile: 'Wellcome to Express Page',
-        currentYear: new Date().getFullYear()
-        
-    })
-})
-
-app.get('/bad', (req, res, next) => {
-    res.send({
-        errorMessage: "unable to access this route"
-    })
-
-})
+app.get("/bad", (req, res, next) => {
+  res.send({
+    errorMessage: "unable to access this route"
+  });
+});
 
 app.listen(5000, () => {
-    console.log('Server is running 5000 port')
-})
+  console.log("Server is running 5000 port");
+});
