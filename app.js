d6a10e1bb7422ed8fde0691572d55e8a25f52602
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-10-29 10:05:22 +0500

    apply mddleware

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const hbs = require("hbs");
+const fs = require("fs");
 
 var app = express();
 hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) {
+      console.log("unable to connect server");
+    }
+  });
+  next();
+});
+app.use((req, res, next) => {
+  res.render("maintaince.hbs");
+});
 app.use(express.static(__dirname + "/public"));
 
 hbs.registerHelper("getCurrentYear", () => {
